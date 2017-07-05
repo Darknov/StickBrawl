1ef4c7e77e1434f32f1e8b3acb1e9964f5686bd8
Darknov <darknov.dono@gmail.com>
2017-07-05 04:09:13 +0200

    better experiment

--- bodypart.js
+++ bodypart.js
@@ -8,7 +8,7 @@ function BodyPart(startingJoint,endingJoint,length) {
         endingJoint.x =  startingJoint.x + cos(startingJoint.arc) * length;
         endingJoint.y = startingJoint.y + sin(startingJoint.arc) * length;
         line(startingJoint.x, startingJoint.y, endingJoint.x, endingJoint.y);
-        ellipse(startingJoint.x, startingJoint.y, 20, 20);
+        ellipse(startingJoint.x, startingJoint.y, 5, 5);
         ellipse(endingJoint.x, endingJoint.y, 5, 5);
 
     }
--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
 var joint1 = new Joint(300,300);
 var joint2 = new Joint();
 var joint3 = new Joint();
+var joint4 = new Joint();
+var joint5 = new Joint();
+var joint6 = new Joint();
 var element1 = new BodyPart(joint1,joint2,40);
 var element2 = new BodyPart(joint2,joint3, 80);
+var element3 = new BodyPart(joint3,joint4, 30);
+var element4 = new BodyPart(joint4,joint5, 50);
+var element5 = new BodyPart(joint5,joint6, 60);
 
 function setup() {
     createCanvas(1366, 768);
@@ -14,6 +20,12 @@ function draw() {
     background('gray');
     joint1.arc += 0.02;
     joint2.arc += 0.05;
+    joint3.arc += 0.01;
+    joint4.arc += 0.03;
+    joint5.arc += 0.1;
     element1.draw();
     element2.draw();
+    element3.draw();
+    element4.draw();
+    element5.draw();
 }
